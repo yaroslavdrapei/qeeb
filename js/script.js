a94f7c7110f1764bbc71c48f0000b39551daeec7
yarikdr <yarikdrapey@gmail.com>
2023-01-02 18:44:56 +0200

    insights

--- js/script.js
+++ js/script.js
@@ -6,14 +6,35 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   const breakpoint = window.matchMedia('(max-width: 830px)')
+  // function move(e, ...rest) {
+  //   const [sel, bp, action1, action2] = rest
+  //   const el = document.querySelector(sel);
+  //   if (e.matches) {
+  //     action1()
+  //   } else {
+  //     action2()
+  //   }
+  // }
   function move(e) {
-    const btn = document.querySelector('.header__btn');
+    const btn = document.querySelector('.header__btn'),
+          title = document.querySelector('.insights__title'),
+          icons = document.querySelectorAll('.post__btn div')
     if (e.matches) {
       document.querySelector('.menu').append(btn.cloneNode(true));
       btn.remove()
+      title.textContent = 'Blog'
+      icons.forEach(icon => {
+        icon.classList.remove('_icon-readmorearrow')
+        icon.classList.add('_icon-readmore')
+      })
     } else {
       document.querySelector('.header__action').append(btn.cloneNode(true));
       btn.remove()
+      title.textContent = 'Insights'
+      icons.forEach(icon => {
+        icon.classList.remove('_icon-readmore')
+        icon.classList.add('_icon-readmorearrow')
+      })
     }
   }
   breakpoint.addListener(move)
